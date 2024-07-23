62621e3faa4a6e6c4b4793108fecaad76e159111
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-07-23 23:57:33 +0900

    Add: navigation Bar to app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex bg-gray-800 text-white p-6 justify-between">
+          <h1 className="text-2xl font-bold">{metadata.title}</h1>
+          <a
+            href="/new"
+            className="hover:bg-blue-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+          >
+            <svg
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="mr-2"
+              aria-hidden="true"
+            >
+                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z"/>
+            </svg>
+            Login
+          </a>
+        </nav>
+        <div className="m-2">{children}</div>
+      </body>
     </html>
   );
 }
